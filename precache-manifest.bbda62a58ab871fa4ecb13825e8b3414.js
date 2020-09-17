16c6e7fee3162666c607c54f332e94e11e3b6e89
dacardenasa <diego.cardenasaleg@gmail.com>
2020-09-17 09:53:25 -0500

    Updates

---
+++ precache-manifest.bbda62a58ab871fa4ecb13825e8b3414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b64184e3c7fd5c5d41a3bd929bd9f91",
+    "url": "/chronometers/index.html"
+  },
+  {
+    "revision": "49768ef4fbb8333cef87",
+    "url": "/chronometers/static/css/main.1984164f.chunk.css"
+  },
+  {
+    "revision": "5551403ed896557d1e79",
+    "url": "/chronometers/static/js/2.ccfccf20.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/chronometers/static/js/2.ccfccf20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49768ef4fbb8333cef87",
+    "url": "/chronometers/static/js/main.a858d9ec.chunk.js"
+  },
+  {
+    "revision": "54646d7675c0a75da4cc",
+    "url": "/chronometers/static/js/runtime-main.0eedfc5a.js"
+  },
+  {
+    "revision": "0bb8692126a813f50f20358eb0fc79c6",
+    "url": "/chronometers/static/media/pattern.0bb86921.jpg"
+  }
+]);
